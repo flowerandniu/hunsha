ba26e3ba9814964d0e52b74a72962d270260001e
7ystar <997002885@qq.com>
2017-04-22 13:00:00 +0800

    有修改

--- js/gx.js
+++ js/gx.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
 	//banner
 	//改变盒子的宽度
-	if($(".bannerxm").height==0){
-		$(".bannerxm").height(1019)//设置初始状态
-	} 
+	$(".bannerxm").height(509)
+	$(window).triggerHandler('resize');		//页面加载完成后resize事件至少执行一次
 	$(window).resize(function(){
 	  let current=$(window).width();
 	  if(current<768){
-	  	 $(".bannerxm").height(function(){
-		    return $('.bannerxm>.screen1>img').height();
-	    });
+	  	console.log($('.bannerxm>.screen1>img').height());
+	  	if($('.bannerxm>.screen1>img').height()!=0){
+		  	 $(".bannerxm").height(function(){
+			    return $('.bannerxm>.screen1>img').height();
+		    });
+		}
 	  }
 	});
-	$(window).triggerHandler('resize');		//页面加载完成后resize事件至少执行一次
 	let j=0;
 	let img1=$('.small>.bannerxm>.screen1');//
 	let letter1=$('.bannerxm .circle div');//
